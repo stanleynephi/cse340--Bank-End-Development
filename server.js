c9ef69247be75685cd930740e600bf8e5cc121fe
stanleynephi <nul23001@byui.edu>
2025-03-08 10:36:56 +0000

    week 01 commit assignment

--- server.js
+++ server.js
@@ -9,11 +9,31 @@ const express = require("express")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+/**adding express jsas our view engine add the express js as a required */
+const expresslayouts = require("express-ejs-layouts")
+
+
+/* ***********************
+ * view engine and templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expresslayouts)
+app.set("layout", "layouts/layout")
+
 
 /* ***********************
  * Routes
- *************************/
+ ************************/
 app.use(static)
+
+/* ***********************
+ * Building the index route for the web application
+ *************************/
+app.get("/", function(req,res){
+  res.render("index", {
+    title: "Home"
+  })
+})
 
 /* ***********************
  * Local Server Information
